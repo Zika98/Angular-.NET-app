ebd556898d09bfea20744e03c7383c1b40d13a44
Srdjan Zivkovic <zivkovic.srki@gmail.com>
2022-11-13 22:59:14 +0100

    Angular, bootstrap
    Made angular proj, implemented bootstrap and called function getUsers from backend.

--- DatingApp/API/Startup.cs
+++ DatingApp/API/Startup.cs
@@ -41,6 +41,7 @@ namespace API
             });
 
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -60,6 +61,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
